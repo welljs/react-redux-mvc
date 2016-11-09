208d93f6db24026d8b83a88406605372fa8b333d
vitalikvo <vitalii.vorobioff@gmail.com>
2016-11-09 19:49:54 +0300

    - helpers added

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,4 @@
+import {PropTypes} from 'react';
 import mergeWith from 'lodash/mergeWith';
 import isArray from 'lodash/isArray';
 export function merge (dst, src) {
@@ -9,19 +10,64 @@ export function merge (dst, src) {
     })}
 }
 
-
-export function warning(message) {
-    /* eslint-disable no-console */
-    if (typeof console !== 'undefined' && typeof console.error === 'function') {
-        console.error(message)
+/**
+ * Returns object for storing current state of async actions
+ * @param {Object || Array} actionTypes - object with pairs of action types CONSTANT:ACTION_NAME, or array of action names
+ * @example
+ * 1.
+ *  const asyncActions = {
+ *      LOAD: LOAD@USER_ACTION,
+ *      LOGIN: LOGIN@USER_ACTION,
+ *      LOGOUT: LOGOUT@USER_ACTION
+ *  }
+ *  const asyncDefaults = createAsyncDefaults(asyncActions);
+ *
+ * 2.
+ * const asyncActions = [LOAD@USER_ACTION, LOGIN@USER_ACTION, LOGOUT@USER_ACTION]
+ * const asyncDefaults = createAsyncDefaults(asyncActions);
+ *
+ * @returns {Object} - returns shape where all keys set in PropTypes.bool.isRequired
+ *
+ */
+export function createAsyncShape(actionTypes) {
+    if (typeof actionTypes !== 'object') {
+        throw new Error(`actions types must be of array or object type, but ${typeof actionTypes} provided`);
     }
-    /* eslint-enable no-console */
-    try {
-        // This error was thrown as a convenience so that if you enable
-        // "break on all exceptions" in your console,
-        // it would pause the execution at this line.
-        throw new Error(message)
-        /* eslint-disable no-empty */
-    } catch (e) {}
-    /* eslint-enable no-empty */
+    if (isArray(actionTypes)) {
+        return actionTypes.reduce((res, actionType) => (res[actionType] = PropTypes.bool.isRequired, res), {});
+    }
+    else {
+        return Object.keys(actionTypes).reduce((res, key) => (res[actionTypes[key]] = PropTypes.bool.isRequired, res), {});
+    }
+}
+
+/**
+ * Returns object for storing current state of async actions
+ * @param {Object || Array} actionTypes - object with pairs of action types CONSTANT:ACTION_NAME, or array of action names
+ * @example
+ * 1.
+ *  const asyncActions = {
+ *      LOAD: LOAD@USER_ACTION,
+ *      LOGIN: LOGIN@USER_ACTION,
+ *      LOGOUT: LOGOUT@USER_ACTION
+ *  }
+ *  const asyncDefaults = createAsyncDefaults(asyncActions);
+ *
+ * 2.
+ * const asyncActions = [LOAD@USER_ACTION, LOGIN@USER_ACTION, LOGOUT@USER_ACTION]
+ * const asyncDefaults = createAsyncDefaults(asyncActions);
+ *
+ * @returns {Object} - returns object where all keys set in false
+ *
+ */
+export function createAsyncDefaults(actionTypes) {
+    if (typeof actionTypes !== 'object') {
+        throw new Error(`actions types must be of array or object type, but ${typeof actionTypes} provided`);
+    }
+    if (isArray(actionTypes)) {
+        return actionTypes.reduce((res, actionType) => (res[actionType] = false, res), {});
+    }
+    else {
+        return Object.keys(actionTypes).reduce((res, key) => (res[actionTypes[key]] = false, res), {});
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {applyReducer} from 'easy-redux';
 export {default as Controller} from './controller';
+export {createAsyncDefaults, createAsyncShape} from './helpers';
 import Model from './model';
 import ReactReduxMvc from './ReactReduxMvc';
 import { withController } from './withController';
