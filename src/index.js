8215bd0d8732eff32cfdfc732fbb9eccdca47c40
Vorobioff <vvorobyev@jtconsulting.ru>
2017-06-23 17:57:35 +0300

    export default added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-export {default as Controller} from './controller';
+import Controller from './controller';
 export {createAsyncDefaults, createAsyncShape} from './helpers';
 import Model from './model';
 import ReactReduxMvc from './ReactReduxMvc';
 import Collection from './collection';
 import { withController } from './withController';
 
-export {Model, withController, ReactReduxMvc, Collection};
+export {Model, withController, ReactReduxMvc, Collection, Controller};
+
+export default {
+  Model, Collection, Controller, withController, ReactReduxMvc
+}
 
 //--------------- create action
